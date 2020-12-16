5b00660cd6931d7b5a6bf46c3cf26ca56e95612b
voromahery <daniel.her@onja.org>
2020-12-16 10:48:25 +0300

    Fixed span

--- dist/country-quiz.e31bb0bc.js
+++ dist/country-quiz.e31bb0bc.js
@@ -28642,7 +28642,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50136" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51688" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
