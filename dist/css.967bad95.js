c2bc97c6201ae64eb7e971d436c7410c7f45a8b2
voromahery <daniel.her@onja.org>
2021-03-13 09:41:52 +0300

    Cleaned file

--- dist/css.967bad95.js
+++ dist/css.967bad95.js
@@ -247,7 +247,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53934" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54091" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
