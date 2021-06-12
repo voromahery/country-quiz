8dc33091e281ced150c36e31c0467a855d63388c
student <daniel.her@onja.org>
2021-06-12 11:50:51 +0300

    fix: removes hover on disabled button

--- dist/css.967bad95.js
+++ dist/css.967bad95.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -144,12 +144,12 @@ function getBundleURL() {
 }
 
 function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
 }
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -184,42 +184,42 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"css/fonts.css":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"css/fonts.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\fonts\\poppins-normal.woff2":[["poppins-normal.8cdf2d84.woff2","fonts/poppins-normal.woff2"],"fonts/poppins-normal.woff2"],"./..\\fonts\\poppins-normal.woff":[["poppins-normal.e8c724f3.woff","fonts/poppins-normal.woff"],"fonts/poppins-normal.woff"],"./..\\fonts\\poppins-medium.woff2":[["poppins-medium.f3a0b5cc.woff2","fonts/poppins-medium.woff2"],"fonts/poppins-medium.woff2"],"./..\\fonts\\poppins-medium.woff":[["poppins-medium.87f61549.woff","fonts/poppins-medium.woff"],"fonts/poppins-medium.woff"],"./..\\fonts\\poppins-semibold.woff2":[["poppins-semibold.264c4215.woff2","fonts/poppins-semibold.woff2"],"fonts/poppins-semibold.woff2"],"./..\\fonts\\poppins-semibold.woff":[["poppins-semibold.0b4167f9.woff","fonts/poppins-semibold.woff"],"fonts/poppins-semibold.woff"],"./..\\fonts\\poppins-bold.woff2":[["poppins-bold.49767633.woff2","fonts/poppins-bold.woff2"],"fonts/poppins-bold.woff2"],"./..\\fonts\\poppins-bold.woff":[["poppins-bold.e7e7e085.woff","fonts/poppins-bold.woff"],"fonts/poppins-bold.woff"],"./..\\fonts\\montserrat-semibold.woff2":[["montserrat-semibold.24b9a2ca.woff2","fonts/montserrat-semibold.woff2"],"fonts/montserrat-semibold.woff2"],"./..\\fonts\\montserrat-semibold.woff":[["montserrat-semibold.93519889.woff","fonts/montserrat-semibold.woff"],"fonts/montserrat-semibold.woff"],"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/default.css":[function(require,module,exports) {
+},{"./../fonts/poppins-normal.woff2":[["poppins-normal.8cdf2d84.woff2","fonts/poppins-normal.woff2"],"fonts/poppins-normal.woff2"],"./../fonts/poppins-normal.woff":[["poppins-normal.e8c724f3.woff","fonts/poppins-normal.woff"],"fonts/poppins-normal.woff"],"./../fonts/poppins-medium.woff2":[["poppins-medium.f3a0b5cc.woff2","fonts/poppins-medium.woff2"],"fonts/poppins-medium.woff2"],"./../fonts/poppins-medium.woff":[["poppins-medium.87f61549.woff","fonts/poppins-medium.woff"],"fonts/poppins-medium.woff"],"./../fonts/poppins-semibold.woff2":[["poppins-semibold.264c4215.woff2","fonts/poppins-semibold.woff2"],"fonts/poppins-semibold.woff2"],"./../fonts/poppins-semibold.woff":[["poppins-semibold.0b4167f9.woff","fonts/poppins-semibold.woff"],"fonts/poppins-semibold.woff"],"./../fonts/poppins-bold.woff2":[["poppins-bold.49767633.woff2","fonts/poppins-bold.woff2"],"fonts/poppins-bold.woff2"],"./../fonts/poppins-bold.woff":[["poppins-bold.e7e7e085.woff","fonts/poppins-bold.woff"],"fonts/poppins-bold.woff"],"./../fonts/montserrat-semibold.woff2":[["montserrat-semibold.24b9a2ca.woff2","fonts/montserrat-semibold.woff2"],"fonts/montserrat-semibold.woff2"],"./../fonts/montserrat-semibold.woff":[["montserrat-semibold.93519889.woff","fonts/montserrat-semibold.woff"],"fonts/montserrat-semibold.woff"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/default.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\background.webp":[["background.1bb78c5c.webp","images/background.webp"],"images/background.webp"],"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/header.css":[function(require,module,exports) {
+},{"./../images/background.webp":[["background.1bb78c5c.webp","images/background.webp"],"images/background.webp"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/header.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/images.css":[function(require,module,exports) {
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/images.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/card.css":[function(require,module,exports) {
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/card.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/buttons.css":[function(require,module,exports) {
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/buttons.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/footer.css":[function(require,module,exports) {
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"css/footer.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -247,7 +247,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49934" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33405" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -278,8 +278,9 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
         assetsToAccept.forEach(function (v) {
           hmrAcceptRun(v[0], v[1]);
         });
-      } else {
-        window.location.reload();
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
       }
     }
 
@@ -422,5 +423,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
 //# sourceMappingURL=/css.967bad95.js.map
